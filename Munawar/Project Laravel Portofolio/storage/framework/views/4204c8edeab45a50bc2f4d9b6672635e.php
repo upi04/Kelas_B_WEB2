f74b9916fa944418886636922f4b37f1b457e4a8
Xenn_ <112091357+Wawan7zz@users.noreply.github.com>
2025-05-11 13:42:23 +0800

    Add files via upload

---
+++ Munawar/Project Laravel Portofolio/storage/framework/views/4204c8edeab45a50bc2f4d9b6672635e.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Biodata</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> 
+</head>
+<body>
+    <h1>Informasi Pribadi</h1>
+    <p><strong>Nama:</strong> <?php echo e($nama); ?></p>
+    <p><strong>NIM:</strong> <?php echo e($nim); ?></p>
+</body>
+</html>
+<?php /**PATH C:\laragon\www\biodata\resources\views/biodata.blade.php ENDPATH**/ ?>
