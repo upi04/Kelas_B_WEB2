e9d92e331e417c12d282cf693ba4d7cb1c5a71bb
zul <muhzulfikarhsrl@gmail.com>
2025-05-21 07:38:37 +0800

    update portofolio - 21-5-2025

---
+++ Muhammad_Zulfikar_Hasrul/Portofolio/resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('content')
+    <h1>About Me</h1>
+    <ol>
+        <ul>Nama: Muhammad Zulfikar Hasrul</ul>
+        <ul>Tempat, Tanggal Lahir: Sungguminasa, 2 Januari 2005</ul>
+        <ul>Hobi: Main Game, Eksplor Terminal Linux dan Badmintoon</ul>
+        <ul>Cita-cita: Menjadi Web Developer</ul>
+    </ol>
+@endsection
---
+++ Muhammad_Zulfikar_Hasrul/Portofolio/resources/views/skills.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('content')
+    <h1>Skills</h1>
+    <ul>
+        <p>Laravel - Tujuan:</p>
+    </ul>
+    <ul>
+        <li>HTML, PHP & Laravel</li>
+        <li>Database Mysql</li>
+        <li>Git & GitHub</li>
+    </ul>
+@endsection
