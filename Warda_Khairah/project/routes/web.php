8d6630730df324cdba25196422a448051228a459
khairahwarda@gmail.com <khairahwarda@gmail.com>
2025-05-21 12:32:46 +0800

    Laporan 2

--- Warda_Khairah/project/routes/web.php
+++ Warda_Khairah/project/routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\View;
 use App\Http\Controllers\BiodataController; 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view(view: 'home');
 });
 
 Route::get('/biodata', [BiodataController::class, 'index']);
@@ -12,9 +13,18 @@ Route::get('/biodata', [BiodataController::class, 'index']);
 Route::get('/about', action: function ():view {
     return view(view: 'about');
 });
-Route::get('/skill', action: function ():view {
-    return view(view: 'skill');
+Route::get('/skills', action: function ():view {
+    return view(view: 'skills');
 });
-Route::get('/about_warda', action: function ():view {
-    return view(view: 'about_warda');
+
+Route::get('/projects', action: function ():view {
+    return view(view: 'projects');
+});
+
+Route::get('/home', action: function ():view {
+    return view(view: 'home');
+});
+
+Route::get('/contact', action: function ():view {
+    return view(view: 'contact');
 });
