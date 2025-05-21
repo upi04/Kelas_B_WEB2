bb253ce5a78c542fe62742040a56deefc323089b
Aisyahica4 <nuraisya995@gmail.com>
2025-05-21 21:01:50 +0800

    Laporan 1

---
+++ Nur_Aisyah_S/projek/app/Http/Controllers/biodataController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class biodataController extends Controller{
+    public function show(){
+        return "Hello Nur Aisya, 60200123073";
+    }
+}
