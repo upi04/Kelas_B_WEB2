0a01675f68501ea4febe9a65cc86c53fe5477572
Faezzz13 <faidzputrasekarkegelapan@gmail.com>
2025-05-22 03:02:02 +0800

    storage directory

--- Faidz_Al-Hafidz/storage/framework/views/380d43a11e4c8750aec4f8cf16ab78fc.php
+++
@@ -1,9 +0,0 @@
-
-
-<?php $__env->startSection('content'); ?>
-    <h1>Contact Me</h1>
-    <p>GitHub: <a href="https://github.com/upi04/Kelas_B_WEB2/tree/40d9c5f5e63d72d80a4f2792b8e81d8db1f814d3/Faidz_Al-Hafidz" target="_blank">Faidz Al-Hafidz</a></p>
-    <p>Email:IjatKopling1945@gmail.com</p>
-    <p>Telepon: 0812345678910</p>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layout', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Laporan_2\resources\views/contact.blade.php ENDPATH**/ ?>
