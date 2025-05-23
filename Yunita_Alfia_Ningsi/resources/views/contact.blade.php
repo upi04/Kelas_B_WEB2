12ea5d9588cfd0ae78e30516c1d2d23aaabf8b5f
yunitaalfia <ns2662331@gmail.com>
2025-05-23 21:05:26 +0800

    laporan dua

---
+++ Yunita_Alfia_Ningsi/resources/views/contact.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contact</title>
+</head>
+<body>
+    <h1>Contact:</h1>
+    <p>Email: ns2662331@gmail.com <br>
+    Ig: @yntaalfngsi_ <br>
+    Wa: 081523611137
+    </p>
+        <ol style="list-style: none; display: flex; gap: 15px;">
+        <li>
+            <a href="/home">Home</a>
+        </li>
+        <li>
+            <a href="/aboutnita">Aboutnita</a>   
+        </li>
+        <li>
+            <a href="/skills">Skill</a>
+        </li>
+        <li>
+            <a href="/projects">Projects</a>
+        </li>
+
+    </ol>
+</body>
+</html>
