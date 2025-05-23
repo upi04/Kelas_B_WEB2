7df0b23811d491ed643d8d5470064620d1548ec5
Aisyahica4 <nuraisya995@gmail.com>
2025-05-23 22:37:41 +0800

    perbaikan

---
+++ Nur_Aisya_S/projek/resources/views/about.blade.php
@@ -0,0 +1,39 @@
+@extends('home')
+
+@section('title', 'About')
+
+@section('content')
+<h3>Daftar Diri</h3>
+<table>
+    <tr>
+        <td>Nama</td>
+        <td>:</td>
+        <td>Nur Aisyah S</td>
+    </tr>
+    <tr>
+        <td>NIM</td>
+        <td>:</td>
+        <td>60200123073</td>
+    </tr>
+    <tr>
+        <td>Kelas</td>
+        <td>:</td>
+        <td>B</td>
+    </tr>
+    <tr>
+        <td>Tempat/Tanggal Lahir</td>
+        <td>:</td>
+        <td>Bontote'ne/04 April 2005</td>
+    </tr>
+    <tr>
+        <td>Hobi</td>
+        <td>:</td>
+        <td>Dengar Musik</td>
+    </tr>
+    <tr>
+        <td>Cita-cita</td>
+        <td>:</td>
+        <td>Ibu Kos</td>
+    </tr>
+</table>
+@endsection
