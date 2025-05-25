d3d5b95ffcfd16dd010f8780dfb376a1ca7fbea0
nurulraehan09 <nurulraehan09@gmail.com>
2025-05-25 11:26:33 +0800

    laporan2

---
+++ Nurul_Raehan/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<!-- resources/views/home.blade.php -->
+@extends('layouts.app')
+
+@section('content')
+    <h1>Welcome to My Portfolio</h1>
+    <p>This is the homepage of my Laravel website.</p>
+@endsection
