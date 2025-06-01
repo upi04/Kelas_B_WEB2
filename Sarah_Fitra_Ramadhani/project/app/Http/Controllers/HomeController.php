04957543ec3f28a37cb58cad07b6a9ae3b9a8f0a
Sarahfitrar <sarahfitraramadhani3010@gmail.com>
2025-06-01 15:21:34 +0800

    Laporan ketiga

--- Sarah_Fitra_Ramadhani/project/app/Http/Controllers/HomeController.php
+++ Sarah_Fitra_Ramadhani/project/app/Http/Controllers/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controller
         $fitur = [
             'Nama' => 'Sarah Fitra Ramadhani',
             'Nama Panggilan' => 'Sarah',
-            'Tempat, Tanggal Lahir' => 'Makassar, 30 Oktober 2005',
+            'Tempat, Tanggal Lahir' => 'Makassar,30 Oktober 2005',
             'Jenis Kelamin' => 'Perempuan',
             'Alamat' => 'Pallangga,Gowa',
             'Email' => 'sarahfitraramadhani3010@gmail.com',
