683e1d5fc55c6ec251bc0b590e0a53832da96409
Aisyahica4 <nuraisya995@gmail.com>
2025-06-16 13:27:52 +0800

    Laporan 4

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+<<<<<<< HEAD
 /*
 |--------------------------------------------------------------------------
 | Console Routes
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
+=======
+>>>>>>> 5882177 (Laporan 4)
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+<<<<<<< HEAD
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\SkillController;
@@ -17,3 +18,18 @@ Route::get('/skills/{id}', [SkillController::class, 'show']);
 
 Route::get('/messages', [MessageController::class, 'index']);
 Route::get('/messages/{id}', [MessageController::class, 'show']);
+=======
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\BookController;
+
+// Halaman utama (opsional)
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route untuk daftar mahasiswa
+Route::get('/students', [StudentController::class, 'index'])->name('students.index');
+
+// Route untuk daftar buku
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+>>>>>>> 5882177 (Laporan 4)
