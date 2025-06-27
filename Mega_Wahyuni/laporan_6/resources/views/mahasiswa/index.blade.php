10f9a1765c8a26a396ba98d7460300805a88e46b
megawahyunii <megawahyuni846@gmail.com>
2025-06-28 03:21:34 +0800

    Tambah laporan 5 dan 6

---
+++ Mega_Wahyuni/laporan_6/resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+
+<h1>Daftar Mahasiswa</h1>
+
+@if(session('success'))
+    <p style="color: green;">{{ session('success') }}</p>
+@endif
+
+<a href="{{ url('/mahasiswa/create') }}">Tambah Mahasiswa</a> | 
+<a href="{{ url('/mahasiswa/trash') }}">Lihat Trash</a><br><br>
+
+<table border="1" cellpadding="8">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Alamat</th>
+            <th>No HP</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+            @forelse($data as $mhs)
+        <tr>
+            <td>{{ $mhs->id }}</td>
+            <td>{{ $mhs->nama }}</td>
+            <td>{{ $mhs->nim }}</td>
+            <td>{{ $mhs->profile->alamat ?? '-' }}</td>
+            <td>{{ $mhs->profile->no_hp ?? '-' }}</td>
+            <td>
+                <a href="{{ url('/mahasiswa/' . $mhs->id . '/edit') }}">
+                    <button type="button">Edit</button>
+                </a>
+
+                <form action="{{ url('/mahasiswa/' . $mhs->id) }}" method="POST" style="display:inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button onclick="return confirm('Yakin hapus?')">Hapus</button>
+                </form>
+            </td>
+        </tr>
+    @empty
+        <tr>
+            <td colspan="6" align="center">Belum ada data.</td>
+        </tr>
+    @endforelse
+    </tbody>
+</table>
+
+</body>
+</html>
+
